4eee9051f612a861fbf6968622b8e2d47ec1ee95
Marvin Santos <marvincenteno23@gmail.com>
2015-09-16 15:45:37 -0300

    correção4

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -3,28 +3,30 @@ public class Elfo{
     private int flechas;
     private int experiencia;
     
-    public Elfo(String n){
-        nome = n;
-        flechas = 42;
-        experiencia = 0;
+    /*type initializer
+     * {
+     *     flechas = 42;
+     *     }
+     */
+    
+    public Elfo(String nome){
+        this(nome,42);
     }     
     
-    public Elfo(String n, int flech){
-        nome = n; 
-        flechas = flech;
-        experiencia = 0;
-               
+    public Elfo(String nome, int flechas){
+        this.nome = nome; 
+        this.flechas = flechas;
+        experiencia = 0;              
     }
     
-    public boolean atingido(){
-        return true;
-    }    
+      
     
     public void atirarFlechaRefactory(){
-    this.flechas--;
+    flechas--;
+    boolean acertar = true;
     
-    if(atingido() == true){
-        this.experiencia += 10;
+    if(acertar){
+        experiencia += 10;
     }
     }   
 }   
