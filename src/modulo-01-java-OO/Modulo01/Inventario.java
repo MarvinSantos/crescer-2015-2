a109f23ef0d358d3d72ca649393bcb84c33227f6
Marvin Santos <marvincenteno23@gmail.com>
2015-09-19 18:59:54 -0300

    Implementado o metodo ordenarItens() e seus testes unitarios

--- src/modulo-01-java-OO/Modulo01/Inventario.java
+++ src/modulo-01-java-OO/Modulo01/Inventario.java
@@ -40,12 +40,37 @@ public class Inventario{
         Item itemMaiorQuantidade = this.getItens().get(0);
         int quantidadeMaior =this.getItens().get(0).getQuantidade();
         
-        for(int i = 0; i < this.getItens().size(); i++){
+        for(int i = 0; i < this.getItens().size() ; i++){
             if(this.getItens().get(i).getQuantidade() > quantidadeMaior){
                 quantidadeMaior = this.getItens().get(i).getQuantidade();
                 itemMaiorQuantidade = this.getItens().get(i); 
             }    
         }
         return itemMaiorQuantidade;
+    }
+    
+    public void ordenarItens(){
+        //variavel que serve pra guardar o item que vai ser realocado em outro lugar
+        Item reservatorio;
+        for(int x = 0; x < this.listaDeItens.size(); x++){
+            for(int i = 0; i < this.listaDeItens.size(); i++){
+                /* se a quantidade do item que varia de acordo com o indice "i" for menor do que a quantidade do item que tem o indice fixo(x)
+                entao o "reservatorio" recebe o item do indice fixo(x) para não perdelo, e o item que tem o indice
+                variando(i) toma o lugar do item que tem o indice fixo(x), depois o "reservatorio" coloca o item de indice fixo(x)
+                no lugar onde estava o item com indice variando(i)*/
+                /* em outras palavras, quando a quantidade do que esta em (i) é menor que a do que esta em (x) , o com a quantidade maior vai pra uma gaveta
+                   separada para dar lugar ao que tem quantidade menor, depois o que esta na gaveta vai para o lugar onde estava o com
+                   quantidade menor */
+               
+                if(this.listaDeItens.get(x).getQuantidade() < this.listaDeItens.get(i).getQuantidade()){
+                    //carregador recebeu o que esta fixo
+                    reservatorio = this.listaDeItens.get(x);
+                    //o lugar do que esta fixo recebe o que esta variando
+                    this.listaDeItens.set(x,this.listaDeItens.get(i));                   
+                    //o lugar no indice que esta variando recebe o item dentro de "resrvatorio" que era o que estava fixo
+                    this.listaDeItens.set(i,reservatorio);
+                }    
+            }    
+        }    
     }    
 }
--- src/modulo-01-java-OO/Modulo01/InventarioTest.java
+++ src/modulo-01-java-OO/Modulo01/InventarioTest.java
@@ -73,11 +73,11 @@ public class InventarioTest
    } 
    
     @Test
-   public void itemComMaiorQuantiadeE1(){
+   public void itemComMaiorQuantiadeIgualA1NoMeioDeItensNegativos(){
        //arrange
-       Item faca = new Item("faca",0);
+       Item faca = new Item("faca",-500);
        Item remedio = new Item("remedio",1);
-       Item desodorante = new Item("desodorante",0);
+       Item desodorante = new Item("desodorante",-6);
        Item carneAssada = new Item("carne assada",0);
        Item itemEsperado = remedio;
        Inventario mochila = new Inventario();
@@ -89,5 +89,112 @@ public class InventarioTest
        Item itemObtido = mochila.getItemComMaiorQuantidade();
        //assert
        assertEquals(itemEsperado, itemObtido);                     
-   }   
+   }
+   
+    @Test
+   public void ordenando15ItensNoInventarioEmOrdemAscendenteDaQualtidade(){
+       //arrange
+       Item faca = new Item("faca",50);
+       Item remedio = new Item("remedio",1);
+       Item desodorante = new Item("desodorante",30);
+       Item balao = new Item("balao",6);
+       Item carneAssada = new Item("carne assada",7);
+       Item carne = new Item("carne",23);
+       Item carro = new Item("carro",8);
+       Item agua = new Item("agua",9);
+       Item bit = new Item("bit",45);
+       Item pc = new Item("pc",36);
+       Item madeira = new Item("madeira",29);
+       Item pedra = new Item("pedra",18);
+       Item ouro = new Item("ouro",200);
+       Item cash = new Item("cash",3);
+       Item dollar = new Item("doallar",72);
+       Inventario mochila = new Inventario();
+       
+       mochila.adcionarItem(faca);
+       mochila.adcionarItem(remedio);
+       mochila.adcionarItem(desodorante);
+       mochila.adcionarItem(carneAssada);
+       mochila.adcionarItem(balao);
+       mochila.adcionarItem(carne);
+       mochila.adcionarItem(carro);
+       mochila.adcionarItem(agua);
+       mochila.adcionarItem(bit);
+       mochila.adcionarItem(pc);
+       mochila.adcionarItem(madeira);
+       mochila.adcionarItem(pedra);
+       mochila.adcionarItem(ouro);
+       mochila.adcionarItem(cash);
+       mochila.adcionarItem(dollar);
+       //act
+       mochila.ordenarItens();
+       //assert
+       assertEquals(remedio,mochila.getItens().get(0));
+       assertEquals(cash,mochila.getItens().get(1));
+       assertEquals(balao,mochila.getItens().get(2));  
+       assertEquals(carneAssada,mochila.getItens().get(3));  
+       assertEquals(carro,mochila.getItens().get(4));  
+       assertEquals(agua,mochila.getItens().get(5));  
+       assertEquals(pedra,mochila.getItens().get(6));
+       assertEquals(carne,mochila.getItens().get(7));
+       assertEquals(madeira,mochila.getItens().get(8)); 
+       assertEquals(desodorante,mochila.getItens().get(9));
+       assertEquals(pc,mochila.getItens().get(10)); 
+       assertEquals(bit,mochila.getItens().get(11)); 
+       assertEquals(faca,mochila.getItens().get(12)); 
+       assertEquals(dollar,mochila.getItens().get(13)); 
+       assertEquals(ouro,mochila.getItens().get(14)); 
+   }
+   
+   @Test
+   public void ordenarItensComQuantidadeNegativa(){
+        //arrange
+       Item faca = new Item("faca",-1);
+       Item remedio = new Item("remedio",-5);
+       Item desodorante = new Item("desodorante",30);
+       Item balao = new Item("balao",6);
+       Inventario mochila = new Inventario();       
+        
+       mochila.adcionarItem(faca);
+       mochila.adcionarItem(remedio);
+       mochila.adcionarItem(desodorante);
+       mochila.adcionarItem(balao);
+       
+       //act
+       mochila.ordenarItens();
+       //assert
+       assertEquals(remedio,mochila.getItens().get(0));
+       assertEquals(faca,mochila.getItens().get(1));
+       assertEquals(balao,mochila.getItens().get(2));  
+       assertEquals(desodorante,mochila.getItens().get(3));  
+       
+   }
+   
+     @Test
+   public void ordenar1Itens(){
+        //arrange
+       Item faca = new Item("faca",-1);  
+       Inventario mochila = new Inventario(); 
+       mochila.adcionarItem(faca);       
+       //act
+       mochila.ordenarItens();
+       //assert
+       assertEquals(faca,mochila.getItens().get(0));       
+   }
+   
+      @Test
+   public void ordenar2Itens(){
+        //arrange
+       Item faca = new Item("faca",2);  
+       Item remedio = new Item("remedio",5);
+       Inventario mochila = new Inventario(); 
+       mochila.adcionarItem(faca);
+       mochila.adcionarItem(remedio);
+       //act
+       mochila.ordenarItens();
+       //assert
+       assertEquals(faca,mochila.getItens().get(0)); 
+       assertEquals(remedio,mochila.getItens().get(1));
+   }  
+   
 }
