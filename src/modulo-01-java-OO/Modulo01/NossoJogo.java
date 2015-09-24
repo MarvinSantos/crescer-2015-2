6ffa45ed7971a06003e6a8cd5dfb65c1d40065b1
Marvin Santos <marvincenteno23@gmail.com>
2015-09-24 17:24:03 -0300

    nova classe NossoJogo

---
+++ src/modulo-01-java-OO/Modulo01/NossoJogo.java
@@ -0,0 +1,13 @@
+
+
+public class NossoJogo
+{
+   
+    public final static String NOME = "BAHH OF THE RINGS";
+    
+    public static void main(String[] args){
+        if(args.length > 0){
+            System.out.println("olá, "+ args[0] +"bem vindo ao" + NOME);
+        }
+    }    
+}
