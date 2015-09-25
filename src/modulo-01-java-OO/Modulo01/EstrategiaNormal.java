b80096867f3916b6547b30825f65f1f6c587815b
Marvin Santos <marvincenteno23@gmail.com>
2015-09-25 17:48:54 -0300

    implementação da classe EstrategiaNormal

---
+++ src/modulo-01-java-OO/Modulo01/EstrategiaNormal.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+//C#
+//public class EstrategiaNormal : EstrategiaDeAtaque 
+public class EstrategiaNormal implements EstrategiaDeAtaque {
+    ArrayList<Elfo> ordemDeAtaque = new ArrayList<>();
+    
+    
+    public void atacar(ExercitoDeElfos exercito , ArrayList<Dwarf> dwarves){
+        ArrayList<Elfo> elfos = exercito.buscar(Status.VIVO);
+        
+        int tamanhoDoPelotaoElfos = elfos.size();
+        int tamanhoDaOrdaDwarves = dwarves.size();
+        int intencoesDeAtaque = tamanhoDoPelotaoElfos * tamanhoDaOrdaDwarves;
+        double trintaPorcentoDasIntencoes = intencoesDeAtaque * 0.3;
+        trintaPorcentoDasIntencoes = Math.floor(trintaPorcentoDasIntencoes);
+        int contadorDeAtaquesDeNoturno = 0;
+        
+        
+        
+         for(Elfo elfo : elfos){
+             
+             if((elfo instanceof ElfoNoturno && contadorDeAtaquesDeNoturno < trintaPorcentoDasIntencoes) || elfo instanceof ElfoVerde){  
+                   ordemDeAtaque.add(elfo);
+             }
+           
+             for(Dwarf dwarf : dwarves){
+               if(elfo instanceof ElfoNoturno && contadorDeAtaquesDeNoturno < trintaPorcentoDasIntencoes){  
+                   elfo.atirarFlecha(dwarf);
+                   contadorDeAtaquesDeNoturno++;
+               }else{
+                   elfo.atirarFlecha(dwarf);
+                }    
+               
+            }  
+        }    
+        
+    }    
+    
+    public ArrayList<Elfo> getOrdemDeAtaque(){
+        return this.ordemDeAtaque;
+    }    
+}
