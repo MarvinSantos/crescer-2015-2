6b42a93bbe47ca54519e7df397e7f6ba04308db9
Marvin Santos <marvincenteno23@gmail.com>
2015-09-25 01:12:27 -0300

    ajuste na classe NossoJogo

--- src/modulo-01-java-OO/Modulo01/NossoJogo.java
+++ src/modulo-01-java-OO/Modulo01/NossoJogo.java
@@ -7,7 +7,7 @@ public class NossoJogo
     
     public static void main(String[] args){
         if(args.length > 0){
-            System.out.println("olá, "+ args[0] +"bem vindo ao" + NOME);
+            System.out.println("olá, "+ args[0] +"bem vindo ao " + NOME);
         }
     }    
 }
