7de4f58e7086feef047d431688552d68625cb093
Marvin Santos <marvincenteno23@gmail.com>
2015-10-17 01:33:35 -0300

    implementação da function daisyGame

---
+++ src/JavaScript/daisyGame.js
@@ -0,0 +1,10 @@
+function daisyGame(numeroDePetalas){
+
+  var resto = numeroDePetalas % 2;
+
+  if ( resto === 1 ){ 
+    return console.log('Love Me')
+  }else {
+    return console.log('Love Me Not')
+  }
+}
