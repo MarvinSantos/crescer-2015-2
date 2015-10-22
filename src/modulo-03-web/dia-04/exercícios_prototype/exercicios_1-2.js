8e8c168f89d062351bde263f1072e7fd0811bf5c
Marvin Santos <marvincenteno23@gmail.com>
2015-10-22 15:22:44 -0200

    ajustes nos exercicios de prototype dia 04

--- src/modulo-03-web/dia-04/exercícios_prototype/exercicios_1-2.js
+++ src/modulo-03-web/dia-04/exercícios_prototype/exercicios_1-2.js
@@ -2,10 +2,10 @@
 
 String.prototype.palindromo = function(){
   var reverso = this.split('').reverse().join('');
-  return this.split('').join('') === reverso;
+  return this.toString() === reverso;
 }
 
 Date.prototype.bissexto = function(){
-  var  ehBissexto = ((this.getFullYear() % 100 !== 0 )&&((this.getFullYear() % 4 === 0) || (this.getFullYear() % 400 === 0))) ? true : false;
-  return ehBissexto;
+  return ((this.getFullYear() % 100 !== 0 && this.getFullYear() % 4 === 0) || (this.getFullYear() % 400 === 0));
+
 }
