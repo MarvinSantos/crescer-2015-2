e8152d4f8b6a5d568842c122f73bf2ff7b9d5758
Marvin Santos <marvincenteno23@gmail.com>
2015-10-22 15:23:51 -0200

    mais um teste pra ano bissexto

--- src/modulo-03-web/dia-04/exercícios_prototype/testes.js
+++ src/modulo-03-web/dia-04/exercícios_prototype/testes.js
@@ -7,3 +7,6 @@ console.assert(data2.bissexto() === false, 'data2 ');
 
 var data3 = new Date(2010,0,1);
 console.assert(data3.bissexto() === false, 'data3');
+
+var data4 = new Date(2000, 0,1);
+console.assert(data4.bissexto() === true, 'data4');
