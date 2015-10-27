1f5db6b6a492a115b63ab090d60db4c8f47dd2c0
Marvin Santos <marvincenteno23@gmail.com>
2015-10-27 01:40:51 -0200

    implementando alguns dos testes Unitarios

--- src/modulo-04-c-sharp/dia-01/ConsoleApp/ConsoleApp.Testes/AgendaTestes.cs
+++ src/modulo-04-c-sharp/dia-01/ConsoleApp/ConsoleApp.Testes/AgendaTestes.cs
@@ -18,7 +18,7 @@ namespace ConsoleApp.Testes
         }
 
         [TestMethod]
-        public void UmContatoEhRemovidoPorNumero()
+        public void DoisContatosSaoRemovidosPorNumero()
         {
             var agenda = new Agenda();
             var contato1 = new Contato("marvinn", 23873);
@@ -29,6 +29,64 @@ namespace ConsoleApp.Testes
             agenda.RemoverContatoPorNumero(23873);
 
             Assert.AreEqual(agenda.quantidadeContatos, 0);
+        }
+
+
+
+        [TestMethod]
+        public void UmContatoEhRemovidoPorNome()
+        {
+            var agenda = new Agenda();
+            var contato1 = new Contato("marvinSantos", 23873);
+            var contato2 = new Contato("marvin", 23873);
+
+            agenda.AdcionarContato(contato1);
+            agenda.AdcionarContato(contato2);
+            agenda.RemoverContatoPorNome("marvinSantos");
+
+            Assert.AreEqual(agenda.quantidadeContatos, 1);
+        }
+
+        [TestMethod]
+        public void Adiciona5ContatosEChamaOMetodoListar()
+        {
+            var agenda = new Agenda();
+            var contato1 = new Contato("marvinSantos", 23873);
+            var contato2 = new Contato("Ben10", 558);
+            var contato3 = new Contato("Goku", 383);
+            var contato4 = new Contato("Bills", 397);
+            var contato5 = new Contato("JavaScript", 844);
+
+            agenda.AdcionarContato(contato1);
+            agenda.AdcionarContato(contato2);
+            agenda.AdcionarContato(contato3);
+            agenda.AdcionarContato(contato4);
+            agenda.AdcionarContato(contato5);
+
+            string textoEsperado = "marvinSantos-23873\nBen10-558\nGoku-383\nBills-397\nJavaScript-844\n";
+            string textoObtido = agenda.ListarContatos();
+            Assert.AreEqual(textoObtido,textoEsperado);
+        }
+
+        [TestMethod]
+        public void Adiciona5ContatosEChamaOMetodoListarContatosOrdenados()
+        {
+            var agenda = new Agenda();
+            var contato1 = new Contato("marvinSantos", 23873);
+            var contato2 = new Contato("Ben10", 558);
+            var contato3 = new Contato("Goku", 383);
+            var contato4 = new Contato("Bills", 397);
+            var contato5 = new Contato("JavaScript", 844);
+
+            agenda.AdcionarContato(contato1);
+            agenda.AdcionarContato(contato2);
+            agenda.AdcionarContato(contato3);
+            agenda.AdcionarContato(contato4);
+            agenda.AdcionarContato(contato5);
+
+            string textoEsperado = "Ben10-558\nBills-397\nGoku-383\nJavaScript-844\nmarvinSantos-23873\n";
+            string textoObtido = agenda.ListarOrdenadosPorNome();
+            Assert.AreEqual(textoObtido, textoEsperado);
         }
     }
 }
