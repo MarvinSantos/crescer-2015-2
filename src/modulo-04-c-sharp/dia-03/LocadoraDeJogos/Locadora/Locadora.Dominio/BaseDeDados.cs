f54c3a7ccca7ba808716c57a511eb5aa14d193ce
Marvin Santos <marvincenteno23@gmail.com>
2015-10-30 01:55:22 -0200

    implementando função de editar jogo e ajustando bugs de outras

--- src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/Locadora/Locadora.Dominio/BaseDeDados.cs
+++ src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/Locadora/Locadora.Dominio/BaseDeDados.cs
@@ -37,7 +37,7 @@ namespace Locadora.Dominio
                   
         }
 
-        public XElement pasquisarPorNome(string nome)
+        public XElement PesquisarPorNome(string nome)
         {
             try
             {
@@ -51,15 +51,31 @@ namespace Locadora.Dominio
              
         }
 
-        public Jogo getJogo(string nomeDoJogo)
+        public Jogo GetJogo(string nomeDoJogo)
         {
-            XElement xejogo = pasquisarPorNome(nomeDoJogo);
+            XElement xejogo = PesquisarPorNome(nomeDoJogo);
             string nomeJogo = xejogo.Element("nome").Value;
             double precoJogo = Convert.ToDouble(xejogo.Element("preco").Value);
             string categoriaJogo = xejogo.Element("categoria").Value;
             int idJogo = Convert.ToInt32(xejogo.Attribute("id").Value);
             Jogo jogo = new Jogo(nomeJogo,categoriaJogo,precoJogo,idJogo);
             return jogo;
+        }
+
+        public void AlterarCamposDoJogo(Jogo jogo, string jogoParaAlterar)
+        {
+            XElement xeJogo = PesquisarPorNome(jogoParaAlterar);
+            xeJogo.Element("nome").Value = jogo.Nome;
+            xeJogo.Element("categoria").Value = jogo.Categoria;
+            xeJogo.Element("preco").Value = Convert.ToString(jogo.Preco);
+            xeJogo.Attribute("id").Value = Convert.ToString(jogo.Id);
+            jogos.Save(caminhoArquivo);
+        }
+
+        public int GetIdJogo(string nomeDoJogo)
+        {
+            XElement xeJogo = PesquisarPorNome(nomeDoJogo);
+            return Convert.ToInt32(xeJogo.Attribute("id").Value);
         }
     }
 }
