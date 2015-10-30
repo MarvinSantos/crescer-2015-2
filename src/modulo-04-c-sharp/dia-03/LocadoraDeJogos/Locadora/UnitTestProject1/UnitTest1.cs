cc84468a2752a512525f3c6c2187bf8dede1003d
Marvin Santos <marvincenteno23@gmail.com>
2015-10-30 01:55:40 -0200

    projetos da locadora

---
+++ src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/Locadora/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Locadora.Dominio;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PesquisandoJogoPorNome()
+        {
+            //consertar
+
+            BaseDeDados dbXml = new BaseDeDados();
+            Jogo jogo = dbXml.GetJogo("Goof Troop");
+            double precoEsperado = 41.9;
+            string informacoesDoJogo = jogo.ToString();
+            string informacoesEsperadas = "Nome: Goof Troop\r\nID: 19\r\nCategoria: AVENTURA\r\nPreço: $41.90 ";
+            
+            Assert.AreEqual(informacoesDoJogo,informacoesEsperadas);
+        }
+    }
+}
