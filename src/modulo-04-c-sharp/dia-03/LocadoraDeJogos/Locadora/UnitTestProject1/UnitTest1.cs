5c2e74894c268d6db02d63a13af014b97f1b081d
Marvin Santos <marvincenteno23@gmail.com>
2015-10-31 03:30:09 -0200

    adicionando alguns testes

--- src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/Locadora/UnitTestProject1/UnitTest1.cs
+++ src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/Locadora/UnitTestProject1/UnitTest1.cs
@@ -13,11 +13,38 @@ namespace UnitTestProject1
 
             BaseDeDados dbXml = new BaseDeDados();
             Jogo jogo = dbXml.GetJogo("Goof Troop");
-            double precoEsperado = 41.9;
             string informacoesDoJogo = jogo.ToString();
             string informacoesEsperadas = "Nome: Goof Troop\r\nID: 19\r\nCategoria: AVENTURA\r\nStatus: Disponivel\r\nPreço: $41.90 ";
             
             Assert.AreEqual(informacoesDoJogo,informacoesEsperadas);
+        }
+
+
+        [TestMethod]
+        public void CadastrandoUmNovoJogo()
+        {
+
+            BaseDeDados dbXml = new BaseDeDados();
+            Jogo jogo = new Jogo("Yugioh","duel cards",109.90,dbXml.nextID(),"locado");
+            dbXml.cadastrarJogo(jogo);
+            Jogo jogoEsperado = new Jogo("Yugioh", "duel cards", 109.90, 21, "locado");
+
+            Assert.AreEqual(jogo, jogoEsperado);
+        }
+
+
+        [TestMethod]
+        public void EditandoUmJogo()
+        {
+            //consertar
+            BaseDeDados dbXml = new BaseDeDados();
+            Jogo jogo = new Jogo("Zelda", "Aventura", 50, dbXml.nextID(), "locado");
+            dbXml.cadastrarJogo(jogo);
+            Jogo jogoEditado = new Jogo("Super Zelda 2", "Aventura", 50, 22, "locado");
+            dbXml.AlterarCamposDoJogo(jogoEditado, "Zelda");
+            Jogo jogoEsperado = dbXml.GetJogo("Super Zelda 2");
+
+            Assert.AreEqual(jogo, jogoEsperado);
         }
     }
 }
