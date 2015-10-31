573297f503668003f084df4d3c8038acfbe09425
Marvin Santos <marvincenteno23@gmail.com>
2015-10-31 03:01:03 -0200

    refatorando

--- src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/Locadora/Locadora.Dominio/Jogo.cs
+++ src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/Locadora/Locadora.Dominio/Jogo.cs
@@ -13,24 +13,26 @@ namespace Locadora.Dominio
         public string Categoria { get; set; }
         public double Preco { get; set; } 
         public int Id { get; set; }
+        public string Status { get; set; }
 
-        public Jogo(string nome, string categoria, double preco,int id)
+        public Jogo(string nome, string categoria, double preco,int id,string status)
         {
             this.Nome = nome;
             this.Categoria = categoria;
             this.Preco = preco;
             this.Id = id;
+            this.Status = status;
         }
 
         public XElement ToXElement()
         {
-            XElement jogoXElemento = new XElement("jogo", new XAttribute("id", Id), new XElement("nome", Nome), new XElement("preco", Preco), new XElement("categoria", Categoria));
+            XElement jogoXElemento = new XElement("jogo", new XAttribute("id", Id), new XElement("nome", Nome), new XElement("preco", Preco), new XElement("categoria", Categoria), new XElement("status",Status));
             return jogoXElemento;
         }
 
         public override string ToString()
         {
-            string informacoes = string.Format("Nome: " + Nome + "\r\nID: "+ this.Id + "\r\nCategoria: " + this.Categoria + "\r\nPreço: {0:C} ",this.Preco);
+            string informacoes = string.Format("Nome: " + Nome + "\r\nID: "+ this.Id + "\r\nCategoria: " + this.Categoria +"\r\nStatus: "+this.Status +"\r\nPreço: {0:C} ",this.Preco);
             return informacoes;
         }
 
