4fa445188ff39e025964643329a93608539f752a
Marvin Santos <marvincenteno23@gmail.com>
2015-11-04 18:16:10 -0200

    classe relatoriio model

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/RelatorioModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class RelatorioModel
+    {
+        public List<JogoModel> Jogos { get; set; }
+
+
+        public RelatorioModel()
+        {
+            Jogos = new List<JogoModel>();
+        }
+
+    }
+}
