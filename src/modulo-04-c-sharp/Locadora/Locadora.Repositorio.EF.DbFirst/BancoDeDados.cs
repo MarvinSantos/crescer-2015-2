fea3574976ab11c3918a203ca9a5055bb8e7f13e
Marvin Santos <marvincenteno23@gmail.com>
2015-11-09 18:04:35 -0200

    classes entity framework

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF.DbFirst/BancoDeDados.cs
@@ -0,0 +1,63 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Repositorio.EF.DbFirst
+{
+    public class BancoDeDados : DbContext
+    {
+        public BancoDeDados() : base("LOCADORA")
+        {
+                
+        }
+
+        public DbSet<Jogo> Jogo { get; set; }
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new JogoMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+    class JogoMap : EntityTypeConfiguration<Jogo>
+    {
+        public JogoMap()
+        {
+            ToTable("jogo");
+            HasKey(p => p.Id);
+
+            Property(p => p.Nome).IsRequired();
+            Property(p => p.Preco).IsRequired();
+            Property(p => p.Descricao).IsRequired();
+            Property(p => p.Imagem).IsOptional();
+            Property(p => p.Video).IsOptional();
+            Property(p => p.Categoria).IsRequired();
+            Property(p => p.Selo).IsRequired();
+
+            HasOptional(p => p.ClienteLocacao).WithRequired().Map(m => m.MapKey("IdClienteLocacao"));
+
+        }
+    }
+
+    class ClienteMap : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteMap()
+        {
+            ToTable("Cliente");
+            HasKey(p => p.Id);
+
+            Property(p => p.Nome).IsRequired();
+        }
+    }
+
+}
---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/JogoRepositorio.cs
@@ -0,0 +1,67 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locadora.Dominio;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Locadora.Repositorio.EF
+{
+    public class JogoRepositorio : IJogoRepositorio
+    {
+        
+
+        public int Atualizar(Jogo entidade)
+        {
+            using (BancoDeDados db = new BancoDeDados())
+            {                
+                
+            }
+        }
+
+        public Jogo BuscarPorId(int id)
+        {
+            using (BancoDeDados db = new BancoDeDados())
+            {
+                Jogo jogo = db.Jogo.Find(id);
+                return jogo;
+            }
+        }
+
+        public IList<Jogo> BuscarPorNome(string nome)
+        {
+            using (IDbConnection conexao = CriarConexao())
+            {
+            }
+        }
+
+        public IList<Jogo> BuscarTodos()
+        {
+            using (IDbConnection conexao = CriarConexao())
+            {
+               
+            }
+        }
+
+        public int Criar(Jogo entidade)
+        {
+            using (IDbConnection conexao = CriarConexao())
+            {
+               
+            }
+        }
+
+        public int Excluir(int id)
+        {
+            using (IDbConnection conexao = CriarConexao())
+            {
+            }
+        }
+
+
+    }
+}
