e7ce3f1183fd170e7703cd26d026c7fef8f2f55b
Marvin Santos <marvincenteno23@gmail.com>
2015-11-10 16:04:57 -0200

    configurações

--- src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Migrations/201511101556010_initiall.Designer.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Migrations/201511101556010_initiall.Designer.cs
@@ -7,13 +7,13 @@ namespace Locadora.Repositorio.EF.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class initial2 : IMigrationMetadata
+    public sealed partial class initiall : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initiall));
         
         string IMigrationMetadata.Id
         {
-            get { return "201511100601223_initial2"; }
+            get { return "201511101556010_initiall"; }
         }
         
         string IMigrationMetadata.Source
---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Migrations/201511101556010_initiall.cs
@@ -0,0 +1,73 @@
+namespace Locadora.Repositorio.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initiall : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cliente",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+
+
+            CreateTable(
+                "dbo.Selo",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: false),
+                    Nome = c.String(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+
+            CreateTable(
+                "dbo.Categoria",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: false),
+                    Nome = c.String(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.jogo",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Nome = c.String(nullable: false),
+                    Preco = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    Categoria = c.Int(nullable: false),
+                    Descricao = c.String(nullable: false),
+                    Imagem = c.String(),
+                    Video = c.String(),
+                    Selo = c.Int(nullable: false),
+                    IdClienteLocacao = c.Int(),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Cliente", t => t.IdClienteLocacao)
+                .ForeignKey("dbo.Selo", t => t.Selo)
+                .ForeignKey("dbo.Categoria", t => t.Categoria)
+                .Index(t => t.Categoria)
+                .Index(t => t.Selo)
+                .Index(t => t.IdClienteLocacao);
+
+        }
+
+        
+
+    public override void Down()
+        {
+            DropForeignKey("dbo.jogo", "IdClienteLocacao", "dbo.Cliente");
+            DropIndex("dbo.jogo", new[] { "IdClienteLocacao" });
+            DropTable("dbo.jogo");
+            DropTable("dbo.Cliente");
+        }
+    }
+}
