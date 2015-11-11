284e0b34ef6239b29ebd4bc3082599b85eca74b0
Marvin Santos <marvincenteno23@gmail.com>
2015-11-11 04:43:08 -0200

    adicionando Controlador de sessão

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Seguranca/Models/ControladorDeSessao.cs
@@ -0,0 +1,29 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Locadora.Web.MVC.Seguranca.Models
+{
+    public class ControladorDeSessao
+    {
+       
+        public static void CriarSessaoUsuario(Usuario usuarioAutenticado)
+        {
+            string[] permicoes = usuarioAutenticado.Permissoes.Select(perm => perm.Nome).ToArray();
+            var usuarioLogadoModel = new UsuarioLogado(usuarioAutenticado.Email, permicoes);
+
+            FormsAuthentication.SetAuthCookie(usuarioAutenticado.Email, true);
+            HttpContext.Current.Session["USUARIO_LOGADO"] = usuarioLogadoModel;
+        }
+
+        public static void LogOut()
+        {
+            FormsAuthentication.SignOut();
+            HttpContext.Current.Session.Abandon();
+        }
+
+    }
+}
