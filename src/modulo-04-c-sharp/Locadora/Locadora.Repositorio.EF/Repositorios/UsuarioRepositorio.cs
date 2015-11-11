e14002ae1288691bfdd917451cb0922aa0386c3f
Marvin Santos <marvincenteno23@gmail.com>
2015-11-11 04:42:44 -0200

    adicionando usuarioRepositorio

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locadora.Dominio;
+
+namespace Locadora.Repositorio.EF.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        public Usuario BuscarUsuarioPorEmail(string email)
+        {
+            Usuario usuario;
+            using (var db = new BancoDeDados())
+            {
+                usuario = db.Usuario.Include("Permissoes").FirstOrDefault(user => user.Email == email);
+                return usuario;
+            }
+        }
+    }
+}
