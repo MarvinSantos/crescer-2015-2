e813c8016d219103d1ecf8fb3f4b2857eaa32300
Marvin Santos <marvincenteno23@gmail.com>
2015-11-11 04:35:02 -0200

    ajustando validações da loginModel

--- src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/ViewModels/LoginModel.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/ViewModels/LoginModel.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Locadora.Web.MVC.ViewModels
+namespace Locadora.Web.MVC.Models
 {
     public class LoginModel
     {
+        [Required]
         public string Senha { get; set; }
+
+        [Required]
         public string Email { get; set; }
     }
 }
