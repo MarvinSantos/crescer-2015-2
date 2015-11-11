c11ef88b16ed18c376381d27298c77efae316752
Marvin Santos <marvincenteno23@gmail.com>
2015-11-11 04:42:09 -0200

    adicionando classe sevicoAutenticacaoUsuario

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Dominio/Servicos/ServicoAutenticacaoUsuario.cs
@@ -0,0 +1,40 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio.Servicos
+{
+    public class ServicoAutenticacaoUsuario
+    {
+        IUsuarioRepositorio Repositorio;
+
+        public ServicoAutenticacaoUsuario(IUsuarioRepositorio repositorio)
+        {
+            this.Repositorio = repositorio;
+        }
+
+        public Usuario AutenticarEBuscar(string email,string senha)
+        {
+            Usuario usuario = Repositorio.BuscarUsuarioPorEmail(email);
+
+            if(usuario == null)
+            {
+                return null;
+            }
+            else
+            {
+                if(usuario.Senha == senha)
+                {
+                    return usuario;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
