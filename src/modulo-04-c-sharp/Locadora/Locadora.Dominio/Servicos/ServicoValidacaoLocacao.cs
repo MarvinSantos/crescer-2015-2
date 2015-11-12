4ada6094e290a105d1601054c11a42a0156b65a1
Marvin Santos <marvincenteno23@gmail.com>
2015-11-12 17:46:35 -0200

    adicionando serviço de validação de locação

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Dominio/Servicos/ServicoValidacaoLocacao.cs
@@ -0,0 +1,40 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio.Servicos
+{
+    public class ServicoValidacaoLocacao
+    {
+
+        private IJogoRepositorio Repositorio;
+       
+
+        public ServicoValidacaoLocacao(IJogoRepositorio repositorio)
+        {
+            this.Repositorio = repositorio;
+            
+        }
+
+        public bool ValidarLocacaoParaCliente(Cliente cliente)
+        {
+            bool podeLocar;
+
+            int quantidadeJogosLocados = Repositorio.BuscarNumeroDeJogosLocados(cliente);
+
+            if(quantidadeJogosLocados >= 3)
+            {
+                podeLocar = false;
+                return podeLocar;
+            }
+            else
+            {
+                podeLocar = true;
+                return podeLocar;
+            }
+        }
+    }
+}
