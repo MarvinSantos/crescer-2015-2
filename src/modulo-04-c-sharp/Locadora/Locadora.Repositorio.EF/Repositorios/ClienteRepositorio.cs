bda61df31485f1cbb70b20a9fd8dfce311c1710d
Marvin Santos <marvincenteno23@gmail.com>
2015-11-12 17:41:41 -0200

    implementando cliente repositorio

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Repositorios/ClienteRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locadora.Dominio;
+
+namespace Locadora.Repositorio.EF.Repositorios
+{
+    public class ClienteRepositorio : IClienteRepositorio
+    {
+        public IList<Cliente> BuscarPorNome(string nome)
+        {
+            using (var db = new BancoDeDados())
+            {
+                return db.Cliente.ToList();
+            }
+        }
+
+    }
+}
