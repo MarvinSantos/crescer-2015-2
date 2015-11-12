ccf54ff5f88e5bc394ed8c86d141564b4c915732
Marvin Santos <marvincenteno23@gmail.com>
2015-11-12 04:30:50 -0200

    criando classe abstrata jogoBase

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/ViewModels/BaseModels/JogoBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.ViewModels.BaseModels
+{
+    public abstract class JogoBase
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+    }
+}
