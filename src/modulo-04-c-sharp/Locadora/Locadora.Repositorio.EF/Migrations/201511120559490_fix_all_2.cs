92fd92a566a6b31058bf85db2fe7b2dfdbc788a6
Marvin Santos <marvincenteno23@gmail.com>
2015-11-12 04:33:10 -0200

    configurações do vs e migrations

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/Migrations/201511120559490_fix_all_2.cs
@@ -0,0 +1,18 @@
+namespace Locadora.Repositorio.EF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fix_all_2 : DbMigration
+    {
+        public override void Up()
+        {
+         
+        }
+        
+        public override void Down()
+        {
+     
+        }
+    }
+}
