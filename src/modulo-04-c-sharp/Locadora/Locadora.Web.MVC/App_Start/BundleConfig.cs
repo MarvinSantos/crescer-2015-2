e1aa628d6ca845db49fb46f1ba9bc98cc189341d
Marvin Santos <marvincenteno23@gmail.com>
2015-11-15 17:35:34 -0200

    implementando bundle de autocomplete.css

--- src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/App_Start/BundleConfig.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/App_Start/BundleConfig.cs
@@ -31,7 +31,12 @@ namespace Locadora.Web.MVC.App_Start
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
-                        "~/Content/site.css"));
+                        "~/Content/site.css",
+                        "~/Content/themes/base/*.css"));
+
+            bundles.Add(new StyleBundle("~/Autocomplete/css").Include(
+                        "~/Content/themes/base/*.css"));
+
         }
 
     }
