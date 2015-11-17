a78eb334d50b8ce68485737af3dca914a41c8b14
Marvin Santos <marvincenteno23@gmail.com>
2015-11-17 04:32:59 -0200

    implementando interface de linked list

---
+++ src/modulo-05-Java/crescerModuloJava/src/br/com/cwi/crescer/interfaces/MinhaLinkedList.java
@@ -0,0 +1,27 @@
+package br.com.cwi.crescer.interfaces;
+
+import java.util.List;
+import br.com.cwi.crescer.Node;
+
+public interface MinhaLinkedList<T> {
+	
+	
+	public void addFirst(T value);
+
+    public T getFirst();
+
+    public T getLast();
+
+    public List<T> list();
+
+    public void addLast(T value);
+
+    public void removeFirst();
+
+    public void add(int index, T value);
+
+    public void remove(int index);
+
+    public Node<T> getNodeByIndex(int index);
+	
+}
