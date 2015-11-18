4df648cefbdb62cdcfb9c3a68d4383d2ff3dc75d
Marvin Santos <marvincenteno23@gmail.com>
2015-11-18 02:57:42 -0200

    adicionando metodos novos e ajustando existentes na classe menu

--- src/modulo-05-Java/crescerModuloJava/src/br/com/cwi/crescer/menus/MenuPrincipal.java
+++ src/modulo-05-Java/crescerModuloJava/src/br/com/cwi/crescer/menus/MenuPrincipal.java
@@ -1,12 +1,14 @@
 package br.com.cwi.crescer.menus;
 
+import br.com.cwi.crescer.LinkedList;
+
 public class MenuPrincipal {
 	
 	public void mostrarOpcoesDeLista(){
 		String cabecalho = "        Escolha o tipo de lista que preferir";
 		String opcao1 = "1- lista encadeada simples";
 		String opcao2 = "2- lista duplamente encadeada";
-		String opcao3 = "3- Sair";
+		String opcao3 = "0- Para SAIR digite 0(zero)";
 		String quebraLinha = "\r\n";
 		System.out.println(cabecalho +quebraLinha+ opcao1 +quebraLinha+ opcao2 +quebraLinha+ opcao3);
 	}
@@ -16,8 +18,23 @@ public class MenuPrincipal {
 		String adicionar = "1- adicionar na lista";
 		String remover = "2- remover da lista";
 		String listar = "3- Ver a lista";
-		String sair = "4- Sair";
+		String exportarTxt = "4- Exportar Txt";
+		String sair = "0- Para SAIR digite 0(zero)";
 		String quebraLinha = "\r\n";
-		System.out.println(cabecalho +quebraLinha+ adicionar +quebraLinha+ remover +quebraLinha+ listar +quebraLinha+ sair);
+		System.out.println(cabecalho +quebraLinha+ adicionar +quebraLinha+ remover +quebraLinha+ listar +quebraLinha+ exportarTxt +quebraLinha+ sair);
+	}
+	
+	public void pedirIndice(){
+		String texto = "digite o indice do nodo que quer remover";
+		System.out.println(texto);
+	}
+	
+	public void pedirValor(){
+		String texto = "digite o valor em texto que quer add na lista";
+		System.out.println(texto);
+	}
+	
+	public void listar(String texto){
+		System.out.println(texto);
 	}
 }
