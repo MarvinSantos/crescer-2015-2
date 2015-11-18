38fb5cec16147690bcfdd8c3f8cde1386523c96e
Marvin Santos <marvincenteno23@gmail.com>
2015-11-18 03:01:09 -0200

    implementando fluxo do programa e tela

--- src/modulo-05-Java/crescerModuloJava/src/br/com/cwi/crescer/Aplicacao.java
+++ src/modulo-05-Java/crescerModuloJava/src/br/com/cwi/crescer/Aplicacao.java
@@ -2,6 +2,7 @@ package br.com.cwi.crescer;
 
 import br.com.cwi.crescer.interfaces.MinhaLinkedList;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 import br.com.cwi.crescer.LinkedList;
@@ -9,9 +10,15 @@ import listaDuplamenteEncadeada.ListaDuplamenteEncadeada;
 import br.com.cwi.crescer.menus.MenuPrincipal;
 
 public class Aplicacao {
-	public static final int OPCAO1 = 1;
-	public static final int OPCAO2 = 2;
-	public static final int SAIR = 3;
+	public static final int SIMPLELIST = 1;
+	public static final int DUPLAMENTEENCADEADA = 2;
+	
+	public static final int ADD = 1;
+	public static final int REMOVER = 2;
+	public static final int LISTAR = 3;
+	public static final int EXPORTARTXT = 4;
+	
+	public static final int SAIR = 0;
 	
 	
     public static void main(String[] args) {
@@ -23,31 +30,67 @@ public class Aplicacao {
     	while(rodando){
     		menu.mostrarOpcoesDeLista();
         	opcao = scan.nextInt();
-        	if(opcao == OPCAO1){
-        		opcao1();
+        	if(opcao == SIMPLELIST){
+        		logicaDoPrograma(SIMPLELIST);
+        	}else if(opcao == DUPLAMENTEENCADEADA){
+        		logicaDoPrograma(DUPLAMENTEENCADEADA);
+        	}else if(opcao == SAIR){
+        		rodando = false;
         	}
     	}
     	
     }
     
-    private static void opcao1(){
-    	final int ADD = 1;
-    	final int REMOVER = 2;
-    	final int LISTAR = 3;
-    	final int SAIR = 4;
+    private static void logicaDoPrograma(int opcaoLista){
+    	MinhaLinkedList<String> linkedList;
+    	if(opcaoLista == SIMPLELIST){
+    		linkedList = new LinkedList<String>();
+    	}else{
+    		linkedList = new ListaDuplamenteEncadeada<String>();
+    	}
+    	
     	MenuPrincipal menu = new MenuPrincipal();
     	Scanner scan = new Scanner(System.in);
-    	MinhaLinkedList<String> linkedList = new LinkedList<String>();
     	boolean rodando = true;
     	int opcao;
     	
     	while(rodando){
     		menu.mostrarMenuManter();
     		opcao = scan.nextInt();
+    		scan.nextLine();
     		if(opcao == ADD ){
-    			//TODO:fazer a logica pra caso o usuario escolha a opcao adicionar
+    			menu.pedirValor();
+    			String valor = scan.nextLine();
+    			if(!valor.isEmpty() && (valor instanceof String)){
+    				linkedList.add(1, valor);
+    			}else{
+    				System.out.println("o valor deve ser do tipo texto");
+    			}
+    		}else if(opcao == REMOVER){
+    			menu.pedirIndice();
+    			try{
+    				int indice = scan.nextInt();
+    				linkedList.remove(indice);
+    			}catch(Exception e){
+    				System.out.println("o valor deve ser do tipo inteiro");
+    			}
+    			
+    		}else if(opcao == LISTAR){
+    			String texto = linkedList.toString();
+    			menu.listar(texto);
+    			
+    		}else if(opcao == EXPORTARTXT){
+    			try{
+    				linkedList.exportarTxtDaLista();
+    			}catch(IOException e){
+    				System.out.println("nao deu pra exportar, tente outra vez");
+    			}
+    			
+    		}else if(opcao == SAIR){
+    			rodando = false;
     		}
     	}
     	
     }
+    
 }
