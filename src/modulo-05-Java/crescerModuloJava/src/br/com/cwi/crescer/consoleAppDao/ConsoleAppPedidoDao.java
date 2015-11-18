8a50ceb22255006f1aa16bd9a480d133c658f195
Marvin Santos <marvincenteno23@gmail.com>
2015-11-18 17:50:21 -0200

    console app de testes para pedido

---
+++ src/modulo-05-Java/crescerModuloJava/src/br/com/cwi/crescer/consoleAppDao/ConsoleAppPedidoDao.java
@@ -0,0 +1,61 @@
+package br.com.cwi.crescer.consoleAppDao;
+
+import java.sql.SQLException;
+
+public class ConsoleAppPedidoDao {
+
+    public static void main(String[] args) throws SQLException {
+
+        // try (Connection conexao = new ConnectionFactory().getConnection()) {
+        // PedidoDao pedidoDao = new PedidoDao();
+        // Pedido pedido = new Pedido();
+        // pedido.setDsPedido("uma nave espacial");
+        //
+        // pedido.setIdCliente(1l);
+        //
+        // pedidoDao.insert(pedido);
+        // } catch (SQLException e) {
+        // e.printStackTrace();
+        // }
+
+        // try (Connection conexao = new ConnectionFactory().getConnection()) {
+        // PedidoDao pedidoDao = new PedidoDao();
+        // pedidoDao.delete(1l);
+        // } catch (SQLException e) {
+        // e.printStackTrace();
+        // }
+
+        // try (Connection conexao = new ConnectionFactory().getConnection()) {
+        // PedidoDao pedidoDao = new PedidoDao();
+        // Pedido pedido = new Pedido();
+        // List<Pedido> lista = pedidoDao.listAll();
+        // System.out.println(lista.get(0).toString());
+        // System.out.println(lista.get(1).toString());
+        //
+        // } catch (SQLException e) {
+        // e.printStackTrace();
+        // }
+
+        // try (Connection conexao = new ConnectionFactory().getConnection()) {
+        // PedidoDao pedidoDao = new PedidoDao();
+        // Pedido pedido = new Pedido();
+        // pedido.setIdCliente(3l);
+        // pedido.setDsPedido("duas geladeiras");
+        // pedido.setIdPedido(2l);
+        // ;
+        //
+        // pedidoDao.update(pedido);
+        // } catch (SQLException e) {
+        // e.printStackTrace();
+        // }
+
+        // Pedido pedido = new Pedido();
+        // pedido.setIdCliente(3l);
+        // pedido.setIdPedido(null);
+        // pedido.setDsPedido(null);
+        // PedidoDao pedidoDao = new PedidoDao();
+        // List<Pedido> lista = pedidoDao.find(pedido);
+
+    }
+
+}
