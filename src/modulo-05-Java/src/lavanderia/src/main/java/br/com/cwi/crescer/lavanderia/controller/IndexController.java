3d1e3fabeb6c0f839c42751d31aeb9693b9ffbdc
Marvin Santos <marvincenteno23@gmail.com>
2015-11-21 23:22:56 -0200

    implementando metodos

--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/controller/IndexController.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/controller/IndexController.java
@@ -1,22 +1,36 @@
-package br.com.cwi.crescer.controller;
+package br.com.cwi.crescer.lavanderia.controller;
+
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.cwi.crescer.service.ClienteService;
-import br.com.cwi.crescer.service.PessoaService;
+import br.com.cwi.crescer.lavanderia.service.CidadeService;
+import br.com.cwi.crescer.lavanderia.service.ClienteService;
+import br.com.cwi.crescer.lavanderia.service.ItemService;
+import br.com.cwi.crescer.lavanderia.service.MaterialService;
+import br.com.cwi.crescer.lavanderia.service.PedidoService;
+import br.com.cwi.crescer.lavanderia.service.ProdutoService;
+import br.com.cwi.crescer.lavanderia.service.ServicoService;
+
 
 @Controller
 public class IndexController {
 	
-	private ClienteService clienteService;
+	//private ClienteService clienteService;
+	//private CidadeService cidadeService;
+	//private ServicoService servicoService;
+	//private MaterialService materialService;
+	//private ProdutoService produtoService;
+	//private PedidoService pedidoService;
+	private ItemService itemService;
 	
 	@Autowired
-	public IndexController(ClienteService clienteService) {
+	public IndexController(ItemService itemService) {
 		super();
-		this.clienteService = clienteService;
+		this.itemService = itemService;
 	}
 
     @RequestMapping(name = "/")
@@ -31,14 +45,38 @@ public class IndexController {
     
     
     @RequestMapping("/testes")
-    public String textes(Model model) {
+    public String testes(Model model) {
 		
-		String mensagem = "pagina para debugar";
-		String nome = clienteService.buscarNome(1L);
+		String nome = "";
+		double valorUnit = 0.00;
+		try{
+			valorUnit = itemService.buscarValorUnitario(1L);  
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		nome = Double.toString(valorUnit);
 			
 		model.addAttribute("nome", nome);
 		
 		return "testes";	
     }
+    
+//    @RequestMapping("/testes")
+//    public String testes(Model model) {
+//		
+//		double valor = 0.00;
+//		try{
+//			valor = produtoService.buscarValor(1L); 
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//		String nome = Double.toString(valor);
+//			
+//		model.addAttribute("nome", nome);
+//		
+//		return "testes";	
+//    }
     
 }
