420587a2dec5578b96cdcb1e7be42353faa81399
Marvin Santos <marvincenteno23@gmail.com>
2015-11-28 04:18:51 -0200

    implementando metodos de persistencia

--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/service/ProdutoService.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/service/ProdutoService.java
@@ -1,21 +1,41 @@
 package br.com.cwi.crescer.lavanderia.service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.cwi.crescer.lavanderia.DTO.ClienteDTO;
+import br.com.cwi.crescer.lavanderia.DTO.ClienteResumoDTO;
+import br.com.cwi.crescer.lavanderia.DTO.ProdutoDTO;
+import br.com.cwi.crescer.lavanderia.DTO.ProdutoEditarDTO;
+import br.com.cwi.crescer.lavanderia.DTO.ProdutoResumoDTO;
+import br.com.cwi.crescer.lavanderia.dao.MaterialDao;
 import br.com.cwi.crescer.lavanderia.dao.ProdutoDao;
+import br.com.cwi.crescer.lavanderia.dao.ServicoDao;
+import br.com.cwi.crescer.lavanderia.domain.Cliente;
+import br.com.cwi.crescer.lavanderia.domain.Material;
 import br.com.cwi.crescer.lavanderia.domain.Produto;
+import br.com.cwi.crescer.lavanderia.domain.Produto.SituacaoProduto;
+import br.com.cwi.crescer.lavanderia.domain.Servico;
+import br.com.cwi.crescer.lavanderia.domain.Cliente.SituacaoCliente;
+import br.com.cwi.crescer.lavanderia.mapper.ClienteMapper;
+import br.com.cwi.crescer.lavanderia.mapper.ProdutoMapper;
 
 @Service
 public class ProdutoService {
 
     private ProdutoDao produtoDao;
+    private MaterialDao materialDao;
+    private ServicoDao servicoDao;
 
     @Autowired
-    public ProdutoService(ProdutoDao produtoDao){
+    public ProdutoService(ProdutoDao produtoDao,MaterialDao materialDao,ServicoDao servicoDao){
         super();
+        this.materialDao = materialDao;
+        this.servicoDao = servicoDao;
         this.produtoDao = produtoDao;
     }
 
@@ -32,4 +52,52 @@ public class ProdutoService {
 
         return valor;
     }
+
+	public void incluir(ProdutoDTO produto) {	
+		Produto produtoASalvar = ProdutoMapper.getNewEntity(produto);
+		Material material = materialDao.findById(produto.getMaterial());
+		Servico servico = servicoDao.findById(produto.getServico());
+		produtoASalvar.setMaterial(material);
+		produtoASalvar.setServico(servico);
+		produtoASalvar.setSituacao(SituacaoProduto.ATIVO);
+		produtoDao.save(produtoASalvar);
+		
+	}
+
+	public List<ProdutoResumoDTO> listarProdutos() {
+		
+		List<Produto> produtos = produtoDao.findAll();
+        List<ProdutoResumoDTO> dtos = new ArrayList<ProdutoResumoDTO>();
+
+        for (Produto produto : produtos) {
+            dtos.add(new ProdutoResumoDTO(produto));
+        }
+
+        return dtos;
+	
+	}
+
+	public ProdutoDTO buscarPorId(Long id) {
+		
+		Produto produto = produtoDao.findById(id);
+	     return ProdutoMapper.toDTO(produto);
+	}
+	
+	
+	public void atualizar(ProdutoEditarDTO produtoEditarDTO) {
+		ProdutoDTO produtoDTO = ProdutoMapper.deEditarDTOtoDTO(produtoEditarDTO);
+        Produto produto= produtoDao.findById(produtoDTO.getIdProduto());
+        ProdutoMapper.merge(produtoDTO, produto);
+       
+        produtoDao.save(produto);
+    }
+
+	public List<SituacaoProduto> listarSituacoes() {
+		ArrayList<SituacaoProduto> situacoes = new ArrayList<SituacaoProduto>();
+        situacoes.add(SituacaoProduto.ATIVO);
+        situacoes.add(SituacaoProduto.INATIVO);
+        return situacoes;
+	}
+	
+	
 }
