a19fcef4e00a228b563154df37100c35def2befe
Marvin Santos <marvincenteno23@gmail.com>
2015-11-28 20:07:57 -0200

    listar e salvar pedido

--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/PedidoDao.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/PedidoDao.java
@@ -1,10 +1,14 @@
 package br.com.cwi.crescer.lavanderia.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.cwi.crescer.lavanderia.domain.Cliente;
 import br.com.cwi.crescer.lavanderia.domain.Pedido;
 
 @Repository
@@ -15,5 +19,22 @@ public class PedidoDao {
 	
 	public Pedido findById(Long id){
 		return em.find(Pedido.class, id);
+	}
+
+	public List<Pedido> findAll() {
+		return em.createQuery("FROM Pedido", Pedido.class).getResultList();
+	}
+
+	@Transactional
+	public Pedido save(Pedido pedido) {
+
+        if (pedido.getIdPedido() == null) {
+            em.persist(pedido);
+
+            return pedido;
+        }
+
+        return em.merge(pedido);
+		
 	}
 }
