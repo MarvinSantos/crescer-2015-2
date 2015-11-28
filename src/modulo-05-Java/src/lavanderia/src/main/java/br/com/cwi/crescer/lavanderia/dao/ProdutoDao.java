86df257f812ab70b09be859724ebd8f4f7673242
Marvin Santos <marvincenteno23@gmail.com>
2015-11-28 04:22:30 -0200

    implementando metodo para salvar no banco

--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ProdutoDao.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ProdutoDao.java
@@ -1,9 +1,10 @@
 package br.com.cwi.crescer.lavanderia.dao;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.cwi.crescer.lavanderia.domain.Produto;
 
@@ -16,5 +17,22 @@ public class ProdutoDao {
 	
 	public Produto findById(Long id){
 		return em.find(Produto.class, id);
+	}
+
+	@Transactional
+	public Produto save(Produto produto) {
+		
+		if (produto.getIdProduto() == null) {
+            em.persist(produto);
+
+            return produto;
+        }
+
+        return em.merge(produto);
+	}
+
+
+	public List<Produto> findAll() {
+		return em.createQuery("FROM Produto", Produto.class).getResultList();
 	}
 }
