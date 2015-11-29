c14dc7345f81a71e803cedf7f7ac9c35f5c31d9a
Marvin Santos <marvincenteno23@gmail.com>
2015-11-29 04:38:34 -0200

    metodos para salvar e listar itens conforme o pedido

--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ItemDao.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ItemDao.java
@@ -1,10 +1,12 @@
 package br.com.cwi.crescer.lavanderia.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
-
+import org.springframework.transaction.annotation.Transactional;
 import br.com.cwi.crescer.lavanderia.domain.Item;
 
 @Repository
@@ -15,5 +17,24 @@ public class ItemDao {
 	
 	public Item findById(Long id) {
 		return em.find(Item.class, id);
+	}
+
+	@Transactional
+	public Item save(Item item) {
+		
+		if (item.getIdItem() == null) {
+            em.persist(item);
+
+            return item;
+        }
+
+        return em.merge(item);
+	}
+
+	public List<Item> findAllByPedido(Long idPedido) {
+		
+		 return em.createQuery("FROM Item i WHERE i.pedido.idPedido = :idPedido", Item.class)
+				 .setParameter("idPedido", idPedido)
+				 .getResultList();
 	}
 }
