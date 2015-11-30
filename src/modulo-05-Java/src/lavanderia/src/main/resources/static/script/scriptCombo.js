33293793b5eb22552e5ea13159551f553759e099
Marvin Santos <marvincenteno23@gmail.com>
2015-11-30 18:33:00 -0200

    script não funcional de combos dependentes

---
+++ src/modulo-05-Java/src/lavanderia/src/main/resources/static/script/scriptCombo.js
@@ -0,0 +1,26 @@
+/**
+ * <!-- 			<script> -->
+// 			<![CDATA[
+// 					$(function(){
+// 						$('#servico').change(function(){
+// 							if( $(this).val() ) {
+// 								$.getJSON('/itens/rest/' + $(this).val(), function(j){
+// 									var options = '<option value=""></option>'; 
+							
+// 									for (var i = 0; i < j.length; i++) {
+// 										console.log(j[i]);
+// 										options += '<option value="' + j[i].idMaterial + '">' + j[i].descricaoMaterial + '</option>';
+									
+// 									}	
+// 									$('#material').html(options).show();
+// 								});
+// 							} 
+// 							else {
+// 								$('#material').html('<option value=""></option>');
+// 							}
+// 						});
+// 					}); 
+// 	            ]]>
+			
+<!-- 			</script> -->
+ */
