8a5c5f85b819cbf4169cd24135abf3e05a6676e1
Marvin Santos <marvincenteno23@gmail.com>
2015-11-30 18:30:59 -0200

    novos metodos para o json

--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/controller/ItemController.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/controller/ItemController.java
@@ -4,22 +4,24 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.cwi.crescer.lavanderia.DTO.ItemDTO;
-import br.com.cwi.crescer.lavanderia.DTO.PedidoResumoDTO;
-import br.com.cwi.crescer.lavanderia.DTO.ProdutoDTO;
+import br.com.cwi.crescer.lavanderia.DTO.ProdutoDTOCombos;
 import br.com.cwi.crescer.lavanderia.domain.Item;
 import br.com.cwi.crescer.lavanderia.domain.Material;
 import br.com.cwi.crescer.lavanderia.domain.Pedido;
-import br.com.cwi.crescer.lavanderia.domain.Produto;
 import br.com.cwi.crescer.lavanderia.domain.Servico;
 import br.com.cwi.crescer.lavanderia.service.ItemService;
 import br.com.cwi.crescer.lavanderia.service.MaterialService;
@@ -31,62 +33,79 @@ import br.com.cwi.crescer.lavanderia.service.ServicoService;
 @RequestMapping("/itens")
 public class ItemController {
 
-	private ItemService itemService;
-	private MaterialService materialService;
-	private ServicoService servicoService;
-	private PedidoService pedidoService;
-	
-	@Autowired
-	public ItemController(ItemService itemService,
-						MaterialService materialService,
-						PedidoService pedidoService,
-						ServicoService servicoService){
-		this.itemService = itemService;
-		this.materialService = materialService;
-		this.pedidoService = pedidoService;
-		this.servicoService = servicoService;
-	}
-	
-	@RequestMapping(path = "/incluir", method = RequestMethod.GET)
+    private ItemService itemService;
+    private MaterialService materialService;
+    private ServicoService servicoService;
+    private PedidoService pedidoService;
+    private ProdutoService produtoService;
+
+    @Autowired
+    public ItemController(ItemService itemService,
+            MaterialService materialService,
+            PedidoService pedidoService,
+            ServicoService servicoService,
+            ProdutoService produtoService) {
+        this.itemService = itemService;
+        this.materialService = materialService;
+        this.pedidoService = pedidoService;
+        this.servicoService = servicoService;
+        this.produtoService = produtoService;
+    }
+
+    @RequestMapping(path = "/incluir", method = RequestMethod.GET)
     public ModelAndView incluir(ItemDTO item){
         return new ModelAndView("item/incluir","item",item);
     }
-	
-	
- 
 
 
-	
-	@RequestMapping(path = "/manter", method = RequestMethod.POST)
+
+
+
+
+    @RequestMapping(path = "/manter", method = RequestMethod.POST)
     public ModelAndView manter(@Valid @ModelAttribute("item")ItemDTO itemDTO,
-    							BindingResult result,
-    							final RedirectAttributes redirectAttributes) {
-		
-		if (result.hasErrors()) {
-		    return new ModelAndView("itens/manter");
-		}
-		
-		Item incluido = itemService.incluirItem(itemDTO);	
-		if(incluido != null){
-			redirectAttributes.addFlashAttribute("menssagemFlash", "incluiu Com Sucesso");
-		}else{
-			redirectAttributes.addFlashAttribute("menssagemFlash", "não foi possivel incluir");
-		}
-		
-		Pedido pedidoAtualizado = pedidoService.atualizarComItem(incluido);
-		
-		ItemDTO outroItem = new ItemDTO();
-		outroItem.setIdPedido(pedidoAtualizado.getIdPedido());
+            BindingResult result,
+            final RedirectAttributes redirectAttributes) {
+
+        if (result.hasErrors()) {
+            return new ModelAndView("itens/manter");
+        }
+
+        Item incluido = itemService.incluirItem(itemDTO);
+        if(incluido != null){
+            redirectAttributes.addFlashAttribute("menssagemFlash", "incluiu Com Sucesso");
+        }else{
+            redirectAttributes.addFlashAttribute("menssagemFlash", "não foi possivel incluir");
+        }
+
+        Pedido pedidoAtualizado = pedidoService.atualizarComItem(incluido);
+
+        ItemDTO outroItem = new ItemDTO();
+        outroItem.setIdPedido(pedidoAtualizado.getIdPedido());
         return new ModelAndView("item/manter","item",outroItem);
     }
-	
-	 @ModelAttribute("materiais")
-	 public List<Material> comboMateriais() {
-	     return materialService.listar();
-	 }
-	    
-	 @ModelAttribute("servicos")
-	 public List<Servico> comboServicos() {
-	     return servicoService.listar();
-	 }
+
+    @ResponseBody
+    @RequestMapping(path = "/rest/{id}")
+    public String buscarServicos(@PathVariable("id") Long id) {
+        JSONArray produtoArray = new JSONArray();
+        for (ProdutoDTOCombos produtoDTOCombos : produtoService.listPorServico(id)) {
+            JSONObject objJSON = new JSONObject();
+            objJSON.put("idMaterial", produtoDTOCombos.getIdMaterial());
+            objJSON.put("materialDescricao", produtoDTOCombos.getDescricaoMaterial());
+            produtoArray.put(objJSON);
+        }
+        return produtoArray.toString();
+    }
+
+
+    @ModelAttribute("materiais")
+    public List<Material> comboMateriais() {
+        return materialService.listar();
+    }
+
+    @ModelAttribute("servicos")
+    public List<Servico> comboServicos() {
+        return servicoService.listar();
+    }
 }
--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ProdutoDao.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/dao/ProdutoDao.java
@@ -1,54 +1,61 @@
 package br.com.cwi.crescer.lavanderia.dao;
 
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import br.com.cwi.crescer.lavanderia.domain.Cliente;
 import br.com.cwi.crescer.lavanderia.domain.Produto;
 
 @Repository
 public class ProdutoDao {
 
-	@PersistenceContext
-	private EntityManager em;
-	
-	
-	public Produto findById(Long id){
-		return em.find(Produto.class, id);
-	}
+    @PersistenceContext
+    private EntityManager em;
 
-	@Transactional
-	public Produto save(Produto produto) {
-		
-		if (produto.getIdProduto() == null) {
+
+    public Produto findById(Long id){
+        return em.find(Produto.class, id);
+    }
+
+    @Transactional
+    public Produto save(Produto produto) {
+
+        if (produto.getIdProduto() == null) {
             em.persist(produto);
 
             return produto;
         }
 
         return em.merge(produto);
-	}
+    }
 
 
-	public List<Produto> findAll() {
-		return em.createQuery("FROM Produto", Produto.class).getResultList();
-	}
+    public List<Produto> findAll() {
+        return em.createQuery("FROM Produto", Produto.class).getResultList();
+    }
 
-	public Produto buscarPorMaterialEServico(Produto produto) {
-		
-		try{
-			Produto produtoBuscado = em.createQuery("FROM Produto p where p.servico = :servico and p.material = :material", Produto.class)
-					.setParameter("material", produto.getMaterial())
-					.setParameter("servico", produto.getServico())
-					.getSingleResult();
-			return produtoBuscado;
-		}catch(javax.persistence.NoResultException e){
-			return null;
-		}
-		 
-		
-	}
+    public Produto buscarPorMaterialEServico(Produto produto) {
+
+        try{
+            Produto produtoBuscado = em.createQuery("FROM Produto p where p.servico = :servico and p.material = :material", Produto.class)
+                    .setParameter("material", produto.getMaterial())
+                    .setParameter("servico", produto.getServico())
+                    .getSingleResult();
+            return produtoBuscado;
+        }catch(javax.persistence.NoResultException e){
+            return null;
+        }
+
+
+    }
+
+    public List<Produto> findListByServico(Long id) {
+        return em.createQuery("FROM Produto p where p.servico.idServico = :idServico", Produto.class)
+                .setParameter("idServico", id)
+                .getResultList();
+    }
 }
