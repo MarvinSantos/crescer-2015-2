48ade7dadbeec4377fd0f2f51cf5c87f60595b75
Marvin Santos <marvincenteno23@gmail.com>
2015-11-30 02:09:37 -0200

    converssoes de classes

--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/mapper/ItemMapper.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/mapper/ItemMapper.java
@@ -1,6 +1,10 @@
 package br.com.cwi.crescer.lavanderia.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.cwi.crescer.lavanderia.DTO.ItemDTO;
+import br.com.cwi.crescer.lavanderia.DTO.ItemListaDTO;
 import br.com.cwi.crescer.lavanderia.domain.Item;
 
 public class ItemMapper {
@@ -10,4 +14,19 @@ public class ItemMapper {
         entity.setPeso(dto.getPeso());   
         return entity;
     }
+
+	public static List<ItemListaDTO> toListaItemListaDTO(List<Item> itens) {
+		List<ItemListaDTO> listaDTO = new ArrayList<ItemListaDTO>();
+		for(Item item : itens){
+			ItemListaDTO dto = new ItemListaDTO();
+			dto.setIdItem(item.getIdItem());
+			dto.setMaterial(item.getProduto().getMaterial().getDescricao());
+			dto.setServico(item.getProduto().getServico().getDescricao());
+			dto.setSituacao(item.getSituacao());
+			dto.setValorTotal(item.getValorTotal());
+			listaDTO.add(dto);
+		}
+		
+		return listaDTO;
+	}
 }
--- src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/mapper/PedidoMapper.java
+++ src/modulo-05-Java/src/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/mapper/PedidoMapper.java
@@ -1,8 +1,8 @@
 package br.com.cwi.crescer.lavanderia.mapper;
 
 import br.com.cwi.crescer.lavanderia.DTO.PedidoDTO;
-import br.com.cwi.crescer.lavanderia.DTO.PedidoResumoDTO;
 import br.com.cwi.crescer.lavanderia.domain.Pedido;
+import br.com.cwi.crescer.lavanderia.DTO.PedidoVisualizarDTO;
 
 public class PedidoMapper {
 	
@@ -18,4 +18,18 @@ public class PedidoMapper {
         entity.setValorFinal(dto.getValorFinal()); 
         return entity;
     }
+
+	public static PedidoVisualizarDTO toPedidoVisualizarDTO(Pedido pedido) {
+		PedidoVisualizarDTO visualizarDTO = new PedidoVisualizarDTO();
+		visualizarDTO.setCliente(ClienteMapper.toClientePedidoDTO(pedido.getCliente()));
+		visualizarDTO.setDataEntrega(pedido.getDataEntrega());
+		visualizarDTO.setDataInclusao(pedido.getDataInclusao());
+		visualizarDTO.setIdPedido(pedido.getIdPedido());
+		visualizarDTO.setItens(ItemMapper.toListaItemListaDTO(pedido.getItens()));
+		visualizarDTO.setSituacao(pedido.getSituacao());
+		visualizarDTO.setValorBruto(pedido.getValorBruto());
+		visualizarDTO.setValorDesconto(pedido.getValorDesconto());
+		visualizarDTO.setValorFinal(pedido.getValorFinal());
+		return visualizarDTO;
+	}
 }
